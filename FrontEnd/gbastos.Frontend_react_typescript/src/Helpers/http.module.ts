df0d05c5ec48bfe2559037e97980dcfce38e2ee2
Gil Antonio Bastos <glbsts@gmail.com>
2023-09-18 11:55:54 -0300

    Update http.module.ts
    Alteração da URL

--- FrontEnd/gbastos.Frontend_react_typescript/src/Helpers/http.module.ts
+++ FrontEnd/gbastos.Frontend_react_typescript/src/Helpers/http.module.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { baseUrl } from '../components/Constants/UrlConstants';
+import { baseUrl } from 'http://localhost:5238/api/GetCategories';
 
 const HttpModule = axios.create({
     baseURL: baseUrl,
