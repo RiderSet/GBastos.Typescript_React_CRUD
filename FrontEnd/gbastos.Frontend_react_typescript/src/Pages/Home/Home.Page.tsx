b5beef3e73584a14dc86b555a97a19cefdeaeecf
Gil Antonio Bastos <glbsts@gmail.com>
2023-09-18 11:59:22 -0300

    Update Home.Page.tsx
    Alteração no teste.

--- FrontEnd/gbastos.Frontend_react_typescript/src/Pages/Home/Home.Page.tsx
+++ FrontEnd/gbastos.Frontend_react_typescript/src/Pages/Home/Home.Page.tsx
@@ -7,14 +7,7 @@ const Home = () => {
          <br />
          <br />
          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fugiat iusto explicabo voluptatum,
-            blanditiis libero eum tempora excepturi inventore labore nesciunt cupiditate commodi beatae. Neque, totam
-            tempore. Esse itaque vitae, reiciendis dolores provident placeat sequi nam pariatur praesentium quaerat
-            autem libero quasi dolorem sapiente aliquid odio dolore maxime repellendus vel incidunt minima? Dolore sequi
-            ea laudantium fuga autem officia, cum fugit rem voluptates, vel quis aperiam consectetur dolor perspiciatis
-            labore. Blanditiis hic eaque natus accusantium officiis quam accusamus. Aperiam sed ullam in. Nisi inventore
-            iste est placeat corrupti? Minima, mollitia hic dolorem porro molestiae nulla itaque incidunt veritatis
-            sunt. Soluta!
+            Isto é um teste.
          </span>
       </div>
    );
